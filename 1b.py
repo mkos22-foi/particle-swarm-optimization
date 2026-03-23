2eec04a5c8d8e74d5a4aec8669abc123dea4515a
mkos22-foi <mkos22@student.foi.hr>
2026-03-23 15:58:41 +0100

    Added global variables

--- 1b.py
+++ 1b.py
@@ -1,5 +1,13 @@
 import math
 
+# Search limits
+u1_min = 0
+u1_max = 512
+u2_min = -100
+u2_max = 1000
+
+
+# Function to be minimized
 def izlaz(u1, u2):
     if u1 > u2:
         return math.sin(u1 + u2**2) * math.exp(abs(1 - math.sqrt(u1**2 + 2.5*u2**2)))
